1cc7e5d5f081df4fab6df8f86175d352ab00850c
Dylan <dcren2010@gmail.com>
2021-06-01 08:54:58 -0400

    set pg credentials in .evn

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,17 @@
-// const { Pool } = require('pg');		Changed to 
+// const { Pool } = require('pg');		Changed to
 import pg from 'pg';
 const { Pool } = pg;
 
-// Connect to PostgreSQL:
-const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'yelp',
-	password: 'postgres123',
-	port: 5433,
-});
+// Connect to PostgreSQL, and if setup .env for credentials, pg is smart enough to get them without write process.env....
+// const pool = new Pool({
+// 	user: 'postgres',
+// 	host: 'localhost',
+// 	database: 'yelp',
+// 	password: '...',
+// 	port: 5433,
+// });
+
+const pool = new Pool();
 
 export default {
 	query: (text, params) => pool.query(text, params),
