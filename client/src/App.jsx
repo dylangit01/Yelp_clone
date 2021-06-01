ffd360c22a8209e62c194946a4504301910c8904
Dylan <dcren2010@gmail.com>
2021-06-01 18:17:05 -0400

    setup routes for front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Details from './routes/Details';
+import Home from './routes/Home';
+import Update from './routes/Update';
 
 const App = () => {
 	return (
-		<>
-			<h1>Hello world</h1>
-		</>
-	)
-}
+		<Router>
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/restaurants/:id/update' component={Update} />
+				<Route exact path='/restaurants/:id' component={Details} />
+			</Switch>
+		</Router>
+	);
+};
 
-export default App
+export default App;
