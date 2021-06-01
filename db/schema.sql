13a3ce5921c9aa00dfa00daf76e6e154f126830e
Dylan <dcren2010@gmail.com>
2021-05-31 22:38:07 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS restaurants CASCADE;
+
+CREATE TABLE restaurants (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(225) NOT NULL,
+	location VARCHAR(225) NOT NULL,
+	price_range INTEGER NOT NULL CHECK(price_range >= 1 and price_range <= 5)
+);
+
+
+INSERT INTO restaurants (name, location, price_range) values ('Wendys', 'New Yorks', 3);
